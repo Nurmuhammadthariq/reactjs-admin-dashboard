f9da24e51ea251e4278f769946661eb96df63f84
thoriqnoer <thoriq.noer@gmail.com>
2021-08-14 15:46:45 +0700

    add data, chart, and widget container

---
+++ src/components/wdigetLg/WidgetLg.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './widgetLg.css';
+
+function WidgetLg() {
+  return <div className="widgetLg">This is widget large</div>;
+}
+
+export default WidgetLg;
---
+++ src/components/widgetSm/WidgetSm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './widgetSm.css';
+
+function WidgetSm() {
+  return <div className="widgetSm">This is widget small</div>;
+}
+
+export default WidgetSm;
