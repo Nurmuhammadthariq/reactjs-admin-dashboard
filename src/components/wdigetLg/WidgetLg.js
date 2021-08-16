df7f17b9910736b9acdc83535902bc4e0d1ab59f
thoriqnoer <thoriq.noer@gmail.com>
2021-08-16 07:00:19 +0700

    Fixing widget small and widget large

--- src/components/wdigetLg/WidgetLg.js
+++ src/components/wdigetLg/WidgetLg.js
@@ -2,7 +2,19 @@ import React from 'react';
 import './widgetLg.css';
 
 function WidgetLg() {
-  return <div className="widgetLg">This is widget large</div>;
+  return (
+    <div className="widgetLg">
+      <h3 className="widgetLgTitle">Latest Transactions</h3>
+      <table className="widgetLgTable">
+        <tr className="widgetLgTr">
+          <th className="widgetLgTh">Customer</th>
+          <th className="widgetLgTh">Date</th>
+          <th className="widgetLgTh">Amount</th>
+          <th className="widgetLgTh">Status</th>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
 export default WidgetLg;
