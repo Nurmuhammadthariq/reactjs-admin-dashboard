570fcbd5dde74a053fd7d158170291e6f5db45b2
thoriqnoer <thoriq.noer@gmail.com>
2021-08-19 06:35:41 +0700

    Fixing Pages in App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
+import UserList from './pages/userList/UserList';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
