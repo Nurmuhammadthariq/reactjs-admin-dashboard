95796c99a240d0ab00ca1c0a262606182df24198
thoriqnoer <thoriq.noer@gmail.com>
2021-08-19 06:36:32 +0700

    Added Link to sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -14,6 +14,7 @@ import {
   WorkOutline,
   Report
 } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
@@ -39,10 +40,12 @@ function Sidebar() {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Quick Menu</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <PermIdentity className="sidebarIcon" />
-              Users
-            </li>
+            <Link to="/users" className="link">
+              <li className="sidebarListItem">
+                <PermIdentity className="sidebarIcon" />
+                Users
+              </li>
+            </Link>
             <li className="sidebarListItem">
               <Storefront className="sidebarIcon" />
               Products
